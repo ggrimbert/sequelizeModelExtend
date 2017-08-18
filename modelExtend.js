a63500ca2b920dab019b48103cd6282cafc99717
Guillaume Grimbert <ggrimbert@kimoce.com>
2017-08-18 14:26:17 +0200

    Syncing good version

--- modelExtend.js
+++ modelExtend.js
@@ -2,69 +2,67 @@ const Model = require('sequelize/lib/model');
 const _ = require('lodash');
  
 /* TODO A REVOIR !*/
-require('sequelize/lib/model').scope = function (option) {
-  const self = class extends this {};
-  let scope;
-  let scopeName;
-  Object.defineProperty(self, 'name', {value: this.name});
+module.exports = function ModelExtend(option) {
+	const self = class extends this {};
+	let scope;
+	let scopeName;
+	Object.defineProperty(self, 'name', {value: this.name});
 
-  self._scope = {};
-  self.scoped = true;
+	self._scope = {};
+	self.scoped = true;
 
-  if (!option) {
+	if (!option) {
 	return self;
-  }
+	}
 
-  const options = _.flatten(arguments);
-  for (const option of options) {
+	const options = _.flatten(arguments);
+	for (const option of options) {
 	scope = null;
 	scopeName = null;
 
-	if (_.isPlainObject(option)) {
-	  if (option.method) {
-		if (Array.isArray(option.method) && !!self.options.scopes[option.method[0]]) {
-		  scopeName = option.method[0];
-		  scope = self.options.scopes[scopeName].apply(self, option.method.slice(1));
-		}
-		else if (self.options.scopes[option.method]) {
-		  scopeName = option.method;
-		  scope = self.options.scopes[scopeName].apply(self);
-		}
-	  } else {
-		scope = option;
-	  }
-	} else {
-	  if (option === 'defaultScope' && _.isPlainObject(self.options.defaultScope)) {
-		scope = self.options.defaultScope;
-	  } else {
-		scopeName = option;
-		scope = self.options.scopes[scopeName];
+		if (_.isPlainObject(option)) {
+			if (option.method) {
+				if (Array.isArray(option.method) && !!self.options.scopes[option.method[0]]) {
+					scopeName = option.method[0];
+					scope = self.options.scopes[scopeName].apply(self, option.method.slice(1));
+				} else if (self.options.scopes[option.method]) {
+					scopeName = option.method;
+					scope = self.options.scopes[scopeName].apply(self);
+				}
+			} else {
+			scope = option;
+			}
+		} else {
+			if (option === 'defaultScope' && _.isPlainObject(self.options.defaultScope)) {
+				scope = self.options.defaultScope;
+			} else {
+				scopeName = option;
+				scope = self.options.scopes[scopeName];
 
-		if (_.isFunction(scope)) {
-		  scope = scope();
-		  require('sequelize/lib/model')._conformOptions(scope, self);
+				if (_.isFunction(scope)) {
+					scope = scope();
+					require('sequelize/lib/model')._conformOptions(scope, self);
+				}
+			}
 		}
-	  }
+
+		if (scope) {
+			_.assignWith(self._scope, scope, (objectValue, sourceValue, key) => {
+				if (key === 'where') {
+					var sourceArray = Array.isArray(sourceValue) ? sourceValue : _.assign(objectValue || {}, sourceValue);
+					if (!objectValue) {
+						objectValue = []; 
+					}
+					return objectValue.concat(sourceArray);
+				} else if (['attributes', 'include'].indexOf(key) >= 0 && Array.isArray(objectValue) && Array.isArray(sourceValue)) {
+					return objectValue.concat(sourceValue);
+				}
+				return objectValue ? objectValue : sourceValue;
+			});
+		} else {
+			throw new sequelizeErrors.SequelizeScopeError('Invalid scope ' + scopeName + ' called.');
+		}
 	}
 
-	if (scope) {
-	  _.assignWith(self._scope, scope, (objectValue, sourceValue, key) => {
-		if (key === 'where') {
-		  var sourceArray = Array.isArray(sourceValue) ? sourceValue : _.assign(objectValue || {}, sourceValue);
-		  if (!objectValue) {
-			objectValue = []; 
-		  }
-		  return objectValue.concat(sourceArray);
-		} else if (['attributes', 'include'].indexOf(key) >= 0 && Array.isArray(objectValue) && Array.isArray(sourceValue)) {
-		  return objectValue.concat(sourceValue);
-		}
-
-		return objectValue ? objectValue : sourceValue;
-	  });
-	} else {
-	  throw new sequelizeErrors.SequelizeScopeError('Invalid scope ' + scopeName + ' called.');
-	}
-  }
-
-  return self;
+	return self;
 };
